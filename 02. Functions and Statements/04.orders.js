c97312c3e683d96cc8c0280992c15db6a24a8b37
Alexander Zashev <azashev93@gmail.com>
2023-03-07 21:00:28 +0200

    Edit orders

--- 02. Functions and Statements/04.orders.js
+++ 02. Functions and Statements/04.orders.js
@@ -17,10 +17,22 @@ function calculatePrice(product, quantity) {
             result = "Invalid";
             break;
     }
-    
+
     console.log(result.toFixed(2));
 }
 
 calculatePrice("water", 5);
 calculatePrice("coffee", 2);
 calculatePrice("neshto", 1);
+
+
+// Task description
+// Write a function that calculates the total price of an order and prints it on the console. The function should receive one of the following products:
+// coffee, coke, water, snacks; and a quantity of the product.
+// The prices for a single piece of each product are: 
+// •	coffee - 1.50
+// •	water - 1.00
+// •	coke - 1.40
+// •	snacks - 2.00
+// 
+// Print the result formatted to the second decimal place.
