1af7cdec7fed41de7a16bfe36fc6b71f17caf6a3
Alexander Zashev <azashev93@gmail.com>
2023-03-07 21:02:01 +0200

    Add description

--- 02. Functions and Statements/06.sign-check.js
+++ 02. Functions and Statements/06.sign-check.js
@@ -8,3 +8,12 @@ console.log(signCheck(5, 12, -15));
 console.log(signCheck(-6, -12, 14));
 console.log(signCheck(-1, -2, -3));
 console.log(signCheck(-5, 1, 1));
+
+// Task description
+// Write a function, that checks whether the result of the multiplication numOne * numTwo * numThree is positive or negative. Try to do this WITHOUT multiplying the 3 numbers.
+// Input:
+// The input comes as parameters named numOne, numTwo, numThree.
+// 
+// Output:
+// • If the result is positive, print "Positive"
+// • Otherwise, print "Negative"
