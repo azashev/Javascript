184881d42776426b3c88dc294ad65e013569d934
Alexander Zashev <azashev93@gmail.com>
2023-03-09 22:53:18 +0200

    Add smallest-of-three-numbers

---
+++ 03. Functions and Statements - Exercise/01.smallest-of-three-numbers.js
@@ -0,0 +1,13 @@
+function smallestNumber(...numbers) {
+    console.log(Math.min(...numbers));
+}
+
+smallestNumber(2, 5, 3);
+smallestNumber(600, 342, 123);
+smallestNumber(25, 21, 4);
+smallestNumber(2, 2, 2);
+
+
+// Task description
+// 
+// Write a function that receives three integers and prints the smallest number
