6a40c4d869640eff75ae15f35cdd4ca205261315
Alexander Zashev <azashev93@gmail.com>
2023-03-10 17:22:44 +0200

    Edit odd-and-even-sum

--- 03. Functions and Statements - Exercise/04.odd-and-even-sum.js
+++ 03. Functions and Statements - Exercise/04.odd-and-even-sum.js
@@ -9,7 +9,6 @@ function oddEvenSum(num) {
 oddEvenSum(1000435);
 oddEvenSum(3495892137259234);
 
-
 // Task description
 // 
 // You will receive a single number. You have to write a function, that returns the sum of all even and all odd digits from that number.
