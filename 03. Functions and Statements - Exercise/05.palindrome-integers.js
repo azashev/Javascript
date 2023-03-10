9547aea3910a8f0d5770e5ff75c84a1a8f054b06
Alexander Zashev <azashev93@gmail.com>
2023-03-10 17:22:53 +0200

    Edit palindrome-integers

--- 03. Functions and Statements - Exercise/05.palindrome-integers.js
+++ 03. Functions and Statements - Exercise/05.palindrome-integers.js
@@ -1,6 +1,5 @@
 function palindromes(numbers) {
     for (const el of numbers) {
-        // console.log(el)
         if (el.toString().split("").reverse().join("") == el) {
             console.log(true);
         } else {
@@ -11,9 +10,11 @@ function palindromes(numbers) {
 
 palindromes([123, 323, 421, 121]);
 
-
-// for (const el of numbers) {
-    // if (el.toString().split("").join().reverse()) {
-        // ;
-    // }
-// }
+// Task description
+// 
+// A palindrome is a number, which reads the same backward as forward, such as 323 or 1001. Write a function, which receives an array of positive integers and checks if 
+// each integer is a palindrome or not.
+// 
+// Output:
+// • If the current integer is a palindrome, print: "true"
+// • Otherwise, print: "false"
