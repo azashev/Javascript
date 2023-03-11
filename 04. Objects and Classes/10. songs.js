332f3527c81388c61a37fff7347712f889b6c579
Alexander Zashev <azashev93@gmail.com>
2023-03-11 19:52:04 +0200

    Add songs

---
+++ 04. Objects and Classes/10. songs.js
@@ -0,0 +1,53 @@
+class Song {
+    constructor(type, name, time) {
+        this.type = type;
+        this.name = name;
+        this.time = time;
+    }
+}
+
+// Example inputs:
+// 
+// input = [3,
+//     'favourite_DownTown_3:14',
+//     'favourite_Kiss_4:16',
+//     'favourite_Smooth Criminal_4:01',
+//     'favourite'];
+
+input = [4,
+    'favourite_DownTown_3:14',
+    'listenLater_Andalouse_3:24',
+    'favourite_In To The Night_3:58',
+    'favourite_Live It Up_3:48',
+    'listenLater'];
+
+// input = [2,
+//     'like_Replay_3:15',
+//     'ban_Photoshop_3:48',
+//     'all'];
+
+let songs = [];
+let numberOfSongs = input.shift();
+let typeOfSong = input.pop();
+
+for (let i = 0; i < numberOfSongs; i++) {
+    let [type, name, time] = input[i].split('_');
+    let song = new Song(type, name, time);
+    songs.push(song);
+}
+
+if (typeOfSong === "all") {
+    songs.forEach((i) => console.log(i.name));
+} else {
+    let filteredSongs = songs.filter((i) => i.type === typeOfSong);
+    filteredSongs.forEach((i) => console.log(i.name));
+}
+
+// Task description
+// 
+// Define a class Song, which holds the following information about songs: typeList, name, and time.
+// You will receive the input as an array.
+// The first element n will be the number of songs.
+// Next n elements will be the songs data in the following format: "{typeList}_{name}_{time}", and the last element will be typeList / "all".
+// Print only the names of the songs, which have the same typeList (obtained as the last parameter).
+// If the value of the last element is "all", print the names of all the songs.
