748f92a52e49fd3e109ef7b628ad0afd6e43c24f
Alexander Zashev <azashev93@gmail.com>
2023-03-11 19:51:10 +0200

    Add person-info

---
+++ 04. Objects and Classes/01. person-info.js
@@ -0,0 +1,12 @@
+function personInfo(firstName, lastName, age) {
+    let person = {
+        firstName,
+        lastName,
+        age,
+    }
+
+    return person;
+}
+
+console.log("Peter", "Pan", "20");
+console.log("George", "Smith", "18");
