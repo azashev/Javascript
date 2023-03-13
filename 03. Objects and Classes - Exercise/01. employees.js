0e1cc0f9abddbd807d07b4718ef0c7609520a72c
Alexander Zashev <azashev93@gmail.com>
2023-03-13 18:25:02 +0200

    Add task description

--- 03. Objects and Classes - Exercise/01. employees.js
+++ 03. Objects and Classes - Exercise/01. employees.js
@@ -24,3 +24,11 @@ employees([
     'Tom Holland'
     ]
 );
+
+// Task description
+// 
+// create a list of employees and their personal numbers.
+// You will receive an array of strings. Each string is an employee name and to assign them a personal number you have to find the length of the name (whitespace included). 
+// Try to use an object.
+// At the end print all the list employees in the following format:
+//  "Name: {employeeName} -- Personal Number: {personalNum}"
